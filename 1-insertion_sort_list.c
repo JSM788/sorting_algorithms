69e4aaba9af461f0ea2fa6cab9ee37c138808ef4
JSM788 <salinasjuan788@gmail.com>
2022-06-14 21:45:54 -0500

    Write a function that sorts a doubly linked list of integers in ascending order

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - check the code
+ * @list: double pointer
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp;
+	int i;
+
+	if (list == NULL)
+		return;
+
+	tmp = *list;
+
+	while (tmp->next != NULL)
+	{
+
+		if (tmp->n > tmp->next->n)
+		{
+			i = tmp->n;
+			*(int *)&tmp->n = tmp->next->n;
+			*(int *)&tmp->next->n = i;
+			print_list(*list);
+
+			while (tmp->prev != NULL)
+			{
+				if (tmp->prev->n > tmp->n)
+				{
+					i = tmp->prev->n;
+					*(int *)&tmp->prev->n = tmp->n;
+					*(int *)&tmp->n = i;
+					print_list(*list);
+				}
+				else
+					break;
+
+				tmp = tmp->prev;
+			}
+		}
+		tmp = tmp->next;
+	}
+}
